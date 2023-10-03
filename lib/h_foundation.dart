c7d5b21de2661e9b3909736421cb297f35f9b3e2
陈浩 <32390273+chenhaoaixuexi@users.noreply.github.com>
2023-10-03 13:16:17 +0800

    M lib/h_foundation.dart R  lib/src/function.debug.dart -> lib/src/h_debug.dart

--- lib/h_foundation.dart
+++ lib/h_foundation.dart
@@ -5,7 +5,7 @@ export 'src/extension/extension.collection.dart';
 export 'src/extension/extension.obj.dart';
 export 'src/extension/extension.string.dart';
 export 'src/extension/extension.widget.dart';
-export 'src/function.debug.dart';
+export 'src/h_debug.dart';
 export 'src/function.misc.dart';
 export 'src/global_conf.dart';
 export 'src/logger.dart';
