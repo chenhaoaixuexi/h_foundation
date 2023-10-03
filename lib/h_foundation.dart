4e994027931191a691f52954f11813d3990427ec
陈浩 <32390273+chenhaoaixuexi@users.noreply.github.com>
2023-10-03 12:35:16 +0800

    M lib/h_foundation.dart A  lib/src/logger.dart  M pubspec.yaml

--- lib/h_foundation.dart
+++ lib/h_foundation.dart
@@ -7,3 +7,4 @@ export 'src/extension.string.dart';
 export 'src/extension.obj.dart';
 export 'src/extension.widget.dart';
 export 'src/function.misc.dart';
+export 'src/logger.dart';
---
+++ lib/src/logger.dart
@@ -0,0 +1,25 @@
+import 'package:logger/logger.dart';
+import 'package:logger_flutter_viewer/logger_flutter_viewer.dart';
+
+Logger logger = Logger(
+  // todo kDebugMode, 环境指定不同的 日志等级
+  output: ScreenOutput(),
+  printer: PrettyPrinter(
+      methodCount: 2, // Number of method calls to be displayed
+      errorMethodCount: 8, // Number of method calls if stacktrace is provided
+      lineLength: 120, // Width of the output
+      colors: true, // Colorful log messages
+      printEmojis: true, // Print an emoji for each log message
+      printTime: false // Should each log print contain a timestamp
+  ),
+);
+
+// shake 未完全 要包一层
+class ScreenOutput extends LogOutput {
+  final ConsoleOutput consoleOutput = ConsoleOutput();
+  @override
+  void output(OutputEvent event) {
+    consoleOutput.output(event);
+    LogConsole.output(event);
+  }
+}
